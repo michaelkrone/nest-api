4aa99e76bb62fc5b54c8ab6ff35d467a1684a3fc
Michael Krone <michael.krone@outlook.com>
2017-12-05 10:34:59 +0100

    refactor: remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 
 import { UserModule } from './user/user.module';
-import { HttpAuthModule } from './http-auth/http-auth.module';
 import { LoginModule } from './login/login.module';
 
 @Module({
